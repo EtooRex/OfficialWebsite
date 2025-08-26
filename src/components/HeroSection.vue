61abf22727b640c57062991cec29c074670d864d
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-26 16:55:25 +0800

    Updated HeroSection.vue

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -20,7 +20,7 @@
               Request Trial Access
             </button>
           <button class="bg-transparent border-2 border-white text-white hover:bg-white/10 font-bold px-6 py-3 rounded-lg transition-colors">
-              Watch Demo
+              Set Up Online Demo
             </button> 
           </div>
         </div>
