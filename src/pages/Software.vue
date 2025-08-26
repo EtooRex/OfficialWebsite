ed5d4541ed92dc99e7334c111ee3f0c4d7585a19
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-26 09:48:17 +0800

    Updated Software.vue

--- src/pages/Software.vue
+++ src/pages/Software.vue
@@ -20,7 +20,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
             </svg>
           </div>
-          <h3 class="text-xl font-bold text-dark mb-3">Getting Started</h3>
+          <h3 class="text-xl font-bold text-dark mb-3">AeROS® (Asset Reliability and Operations Simulation)</h3>
           <p class="text-gray-600 mb-4">
             Learn the basics of AEROS and get up and running quickly with our step-by-step guides.
           </p>
