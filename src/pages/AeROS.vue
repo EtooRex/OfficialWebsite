e2ca22bbb0ef81e688964f881ad6a8514d2e138b
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-26 14:42:44 +0800

    Updated AeROS.vue

--- src/pages/AeROS.vue
+++ src/pages/AeROS.vue
@@ -58,7 +58,7 @@
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              <span>Managing Spare Parts Inventory</span>
+              <span>Debottlenecking Production</span>
             </li>
           </ul>
     
