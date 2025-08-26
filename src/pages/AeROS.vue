dcfef597e8bec6e94472defeab34e1b953dcb929
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-26 14:40:41 +0800

    Updated AeROS.vue

--- src/pages/AeROS.vue
+++ src/pages/AeROS.vue
@@ -28,19 +28,19 @@
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              <span>Reliability Block Diagram modeling</span>
+              <span>System Design Comparison</span>
             </li>
             <li class="flex items-start">
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              <span>Monte Carlo simulation engine</span>
+              <span>Identifying Bottlenecks Early</span>
             </li>
             <li class="flex items-start">
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              <span>Advanced maintenance modeling</span>
+              <span>Setting Realistic Performance Targets</span>
             </li>
           </ul>
     
