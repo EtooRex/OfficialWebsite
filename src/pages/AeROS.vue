11df195a15c7a672fd11cd509b8de7cb7a8ea418
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-26 14:41:37 +0800

    Updated AeROS.vue

--- src/pages/AeROS.vue
+++ src/pages/AeROS.vue
@@ -34,7 +34,7 @@
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              <span>Identifying Bottlenecks Early during FEED</span>
+              <span>Identifying Bottlenecks Early </span>
             </li>
             <li class="flex items-start">
               <svg class="w-6 h-6 text-primary mr-2 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
