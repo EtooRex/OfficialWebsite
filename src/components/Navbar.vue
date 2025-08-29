c4c2dcfe8e3fa756058f2576624e16263ac06557
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-29 14:20:00 +0800

    Updated Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -145,7 +145,7 @@ const isHomePage = computed(() => route.path === '/')
 
 const navItems = [
   { name: 'Services', href: '/services' },
-  { name: 'Learn', href: '/docs/guide' },
+  { name: 'Learn', href: '/docs/index' },
   { name: 'Download', href: '/download' }
 ]
 
