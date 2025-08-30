dc458f4e006c75224068c0776c9bb966d2532d39
EtooRex <103869219+EtooRex@users.noreply.github.com>
2025-08-30 11:55:32 +0800

    Updated vite.config.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '*.md' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/], // <-- allows Vue to compile Markdown files
-    }),
-    Markdown({
-      headEnabled: true,
-      markdownItOptions: {
-        html: true,
-        linkify: true,
-        typographer: true,
-      },
-    })
+    vue()
   ]
 })
